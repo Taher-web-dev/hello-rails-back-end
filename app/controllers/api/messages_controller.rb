c624928507c7a96bf14e4462f1b6b2cf024633f1
Taher <ttaherhagui@gmail.com>
2022-03-22 11:39:31 +0100

    Add code for message controller to display greeting message in the API path.

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,8 @@
 class Api::MessagesController < ApplicationController
+    def index
+        messages_ids = Message.ids
+        random_greeting_id = messages_ids.sample
+        random_greeting = Message.find(random_greeting_id).greeting
+        render json: {message: random_greeting}
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   def index
+    messages_ids = Message.ids
+    random_greeting_id = messages_ids.sample
+    random_greeting = Message.find(random_greeting_id).greeting
+    render json: {message: random_greeting}
   end
 end
